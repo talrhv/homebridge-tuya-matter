b2b1b2ff67e3e9ea173c76414535c998f2597221
Tal <talrhv1@gmail.com>
2026-03-29 00:42:10 +0300

    details edit

--- src/settings.mjs
+++ src/settings.mjs
@@ -6,8 +6,8 @@
  * - config.schema.json "pluginAlias" / platform name
  */
 
-const PLATFORM_NAME = "TuyaPlatform";
-const PLUGIN_NAME = "homebridge-tuya-platform-talrhvfork";
+const PLATFORM_NAME = "TuyaPlatformMatter";
+const PLUGIN_NAME = "homebridge-tuya-matter";
 
 export default {
   PLATFORM_NAME,
